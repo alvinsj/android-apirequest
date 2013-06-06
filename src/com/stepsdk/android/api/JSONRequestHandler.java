28c9bdb0222e173a27c4fd64ded2f138b7ec2025
alvin <alvin@buuuk.com>
2013-06-06 15:44:52 +0800

    throw exception instead for JSON response

--- src/com/stepsdk/android/api/JSONRequestHandler.java
+++ src/com/stepsdk/android/api/JSONRequestHandler.java
@@ -55,7 +55,7 @@ public abstract class JSONRequestHandler extends APIRequestHandler {
     	}
     }
     
-    public abstract void onResponse(HttpEntity response, JSONObject jsonResponse);
+    public abstract void onResponse(HttpEntity response, JSONObject jsonResponse) throws Exception;
 
     public JSONObject parseJSONObject(HttpEntity ent) throws JSONResponseError {
         String result = null;
@@ -66,14 +66,16 @@ public abstract class JSONRequestHandler extends APIRequestHandler {
                 
                 if(result.startsWith("["))
                     result = "{\"array\":"+result+"}";
-                //else
-                JSONObject json = new JSONObject(result);
+                
+                JSONObject json = new JSONObject();
+                if(result.length() > 0 )
+                	json = new JSONObject(result);
                 
                 return json;
             } catch (Exception e) {
-                if(result != null)
-                    Log.d(TAG, "Error JSON format: "+result);
-                //Log.e(TAG, "Error Parsing to JSON: " + e.getMessage());
+                if(result != null && result.length() > 0)
+                    Log.e(TAG, "Error JSON format: "+result);
+                e.printStackTrace();
                 throw new JSONResponseError(e.getMessage());
             }
         }
